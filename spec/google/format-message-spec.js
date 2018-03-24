de106569603d78ab8fc95272354cf7b68231fcab
codesketch <quirino.brizi@gmail.com>
2018-03-24 22:03:11 +0000

    Allow add suggestions and linOutSuggestion

--- spec/google/format-message-spec.js
+++ spec/google/format-message-spec.js
@@ -37,9 +37,126 @@ describe('Google message format', () => {
               ssml: undefined,
               displayText: undefined
             }
-          }]
+          }],
+          suggestions: [],
+          linkOutSuggestion: {}
         }
       },
+      customPushMessage: {},
+      isInSandbox: false
+    });
+  });
+
+  it('should set response with expectedInputs', () => {
+    let testObj = new Formatter();
+    testObj.expectUserResponse();
+
+    expect(testObj.present([testObj.simpleResponseBuilder().setTextToSpeech('text').done()]).get()).toEqual({
+      conversationToken: {},
+      userStorage: '',
+      resetUserStorage: false,
+      expectUserResponse: true,
+      expectedInputs: [{
+        inputPrompt: {
+          richInitialPrompt: {
+            items: [{
+              simpleResponse: {
+                textToSpeech: 'text',
+                ssml: undefined,
+                displayText: undefined
+              }
+            }],
+            suggestions: [],
+            linkOutSuggestion: {}
+          },
+          noInputPrompts: []
+        },
+        possibleIntents: [{
+          intent: 'actions.intent.TEXT'
+        }]
+      }],
+      customPushMessage: {},
+      isInSandbox: false
+    });
+  });
+
+  it('should set response with expectedInputs with suggestions', () => {
+    let testObj = new Formatter();
+    testObj.expectUserResponse();
+
+    expect(testObj
+      .present([testObj.simpleResponseBuilder().setTextToSpeech('text').done()])
+      .suggestions(['More'])
+      .get()).toEqual({
+      conversationToken: {},
+      userStorage: '',
+      resetUserStorage: false,
+      expectUserResponse: true,
+      expectedInputs: [{
+        inputPrompt: {
+          richInitialPrompt: {
+            items: [{
+              simpleResponse: {
+                textToSpeech: 'text',
+                ssml: undefined,
+                displayText: undefined
+              }
+            }],
+            suggestions: [{
+              title: 'More'
+            }],
+            linkOutSuggestion: {}
+          },
+          noInputPrompts: []
+        },
+        possibleIntents: [{
+          intent: 'actions.intent.TEXT'
+        }]
+      }],
+      customPushMessage: {},
+      isInSandbox: false
+    });
+  });
+
+  it('should set response with expectedInputs with suggestions and link out suggetion', () => {
+    let testObj = new Formatter();
+    testObj.expectUserResponse();
+
+    expect(testObj
+      .present([testObj.simpleResponseBuilder().setTextToSpeech('text').done()])
+      .suggestions(['More'])
+      .linkOutSuggestion('Test', 'http://test.com')
+      .get()).toEqual({
+      conversationToken: {},
+      userStorage: '',
+      resetUserStorage: false,
+      expectUserResponse: true,
+      expectedInputs: [{
+        inputPrompt: {
+          richInitialPrompt: {
+            items: [{
+              simpleResponse: {
+                textToSpeech: 'text',
+                ssml: undefined,
+                displayText: undefined
+              }
+            }],
+            suggestions: [{
+              title: 'More'
+            }],
+            linkOutSuggestion: {
+              destinationName: 'Test',
+              openUrlAction: {
+                url: 'http://test.com'
+              }
+            }
+          },
+          noInputPrompts: []
+        },
+        possibleIntents: [{
+          intent: 'actions.intent.TEXT'
+        }]
+      }],
       customPushMessage: {},
       isInSandbox: false
     });
